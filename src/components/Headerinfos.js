8dc7065a52dc52f8b4d65de3b877a4d8a6bcf0b6
Cyril Derc <cyril_derc@hotmail.com>
2023-08-14 16:51:15 +0200

    APP.JS - creation du header

---
+++ src/components/Headerinfos.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Headerinfos = () => {
+  return (
+    <div className="header-container">
+      <h1>HEADER</h1>
+    </div>
+  );
+};
+
+export default Headerinfos;
