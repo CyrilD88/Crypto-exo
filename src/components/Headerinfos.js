38636cc70856f861179af20bed0417fa6ef12216
Cyril Derc <cyril_derc@hotmail.com>
2023-08-14 20:35:28 +0200

    HEADERINFOS - création du composant servant à donner les premières infos

--- src/components/Headerinfos.js
+++ src/components/Headerinfos.js
@@ -1,9 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Headerinfos = () => {
+  const [headerData, setHeaderData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://api.coingecko.com/api/v3/global")
+      .then((res) => setHeaderData(res.data.data));
+  }, []);
+
   return (
     <div className="header-container">
-      <h1>HEADER</h1>
+      <ul className="title">
+        <li>
+          <h1>
+            <img src="./assets/logo.png" alt="logo" /> Watch Tower
+          </h1>
+        </li>
+        <li>
+          {/* toLocaleString permet de mettre un séparateur de milliers */}
+          Crypto-monnaies :
+          {headerData.active_cryptocurrencies &&
+            headerData.active_cryptocurrencies.toLocaleString()}
+        </li>
+        <li>Marché : {headerData.markets && headerData.markets}</li>
+      </ul>
+      <ul className="infos-mkt">
+        <li className="global-mkt">
+          Global Market Cap : <strong></strong>
+        </li>
+      </ul>
     </div>
   );
 };
