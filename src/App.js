e1fc9f032c84aba87850700c00e3d99321168b19
Cyril Derc <cyril_derc@hotmail.com>
2023-08-23 16:45:07 +0200

    Globalchart.js - Création du composant GlobalChart

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Headerinfos from "./components/Headerinfos";
+import GlobalChart from "./components/GlobalChart";
 
 const App = () => {
   return (
     <div className="app-container">
       <header>
         <Headerinfos />
+        <GlobalChart />
       </header>
     </div>
   );
