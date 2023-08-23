a30bab139ca78682bdea71c333bc7382ff88b141
Cyril Derc <cyril_derc@hotmail.com>
2023-08-23 20:28:37 +0200

    GlobalChart.js

--- src/components/GlobalChart.js
+++ src/components/GlobalChart.js
@@ -1,9 +1,53 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Tooltip, Treemap } from "recharts";
 
-const GlobalChart = () => {
+const GlobalChart = ({ coinsData }) => {
+  const [dataArray, setDataArray] = useState([]);
+
+  useEffect(() => {
+    let chartData = [];
+
+    if (coinsData.lenght > 0) {
+      for (let i = 0; i < 45; i++) {
+        chartData.push({
+          name:
+            coinsData[i].symbol.toUpperCase() +
+            " " +
+            coinsData[i].market_cap_change_percentage_24h.toFixed(1) +
+            "%",
+          size: coinsData[i].market_cap,
+          fill: null,
+        });
+      }
+    }
+
+    setDataArray(chartData);
+  }, [coinsData]);
+
+  const TreemapToolTip = ({ active, payload }) => {
+    if (active && payload && payload.length) {
+      return (
+        <div className="custom-tooltip">
+          <p className="label">{payload[0].payload.name}</p>
+        </div>
+      );
+    }
+    return null;
+  };
+
   return (
     <div className="global-chart">
-      <h1>TEST</h1>
+      <Treemap
+        width={730}
+        height={181}
+        data={dataArray}
+        dataKey="size"
+        stroke="rgb(51,51,51)"
+        fill="black"
+        aspectRatio="1"
+      >
+        <Tooltip content={<TreemapToolTip />} />
+      </Treemap>
     </div>
   );
 };
